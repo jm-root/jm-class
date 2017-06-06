eb922bef7f4fe846eaf51961f02b35eb15a120d9
jeff <jeff@jamma.cn>
2017-06-06 13:23:51 +0800

    jm.use

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {Class, moduleClass} from './class';
 import {Obj, moduleObj} from './object';
 
 if (typeof global !== 'undefined' && global) {
-    if (global.jm) {
+    if (global.jm && global.jm.use) {
         global.jm
             .use(moduleClass)
             .use(moduleObj)
